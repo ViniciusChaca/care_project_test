d944f9e8eea4acc7b4946865a74cdd87474fee88
Vinicius Ferreira <67805866+ViniciusChaca@users.noreply.github.com>
2022-06-23 00:10:27 -0300

    Update config-system.sh

--- config-system.sh
+++ config-system.sh
@@ -3,6 +3,7 @@ sudo usermod -a -G www-data vscode
 sudo usermod -a -G vscode www-data
 sudo chmod -R g+w ./
 sudo chmod -R g+w /etc/apache2/sites-available/
+sudo chown -R www-data:www-data -R /etc/apache2/sites-available/
 
 #Substituição de dados para configuração do Apache
 sed 's+WORKSPACE_PATH+'$PWD'+g' ./.devcontainer/apache-site.conf > /etc/apache2/sites-available/000-default.conf
